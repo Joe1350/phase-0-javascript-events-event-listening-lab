18381154e5939ab04edfd89906be508f09a55da0
Joseph Martin <josephmartin1350@gmail.com>
2021-11-06 01:32:34 -0600

    redone

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
+const input = document.querySelector('input');
+
+function addingEventListener() {
+    input.addEventListener('click', function(event) {
+        return 'I was clicked.'
+    })
+}
+
+
+
+
+
+// // const input = document.getElementById('input');
+
+// // function addingEventListener() {
+// //     alert('I was clicked!');
+// //     input.addEventListener('click', addingEventListener)
+// // }
+
+
+
+// // const input = document.getElementById('input');
+
+// // function clickAlert() {
+// //     alert('I was Clicked');
+// // }
+
+// // input.addEventListener('click', clickAlert);
+
 // const input = document.getElementById('input');
 
 // function addingEventListener() {
-//     alert('I was clicked!');
-//     input.addEventListener('click', addingEventListener)
+//     alert('I was clicked.');
+//     input.addEventListener('click', addingEventListener);
 // }
-
-
-
-// const input = document.getElementById('input');
-
-// function clickAlert() {
-//     alert('I was Clicked');
-// }
-
-// input.addEventListener('click', clickAlert);
-
-const input = document.getElementById('input');
-
-function addingEventListener() {
-    alert('I was clicked.');
-    input.addEventListener('click', addingEventListener);
-}
 
