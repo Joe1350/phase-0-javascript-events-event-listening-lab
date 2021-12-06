9cb39438de00bec5260ead06fc9d9c3319bda718
Joseph Martin <josephmartin1350@gmail.com>
2021-12-06 16:13:47 -0700

    commit tree 438fed70a468f11342c05776ff8a4b677e8a3dd3

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const input = document.querySelector('input');
+// const input = document.getElementById('input')
+// input.addEventListener('click', function() {
+//     alert('I was clicked!')
+// })
+
+const input = document.getElementById('input')
+
+function clickAlert() {
+  alert('I was clicked!')
+}
 
 function addingEventListener() {
-    input.addEventListener('click', function(event) {
-        return 'I was clicked.'
-    })
+    input.addEventListener('click', clickAlert)
 }
+addingEventListener();
+
+
+
+
+
+// const input = document.querySelector('input');
+
+// function addingEventListener() {
+//     input.addEventListener('click', function(event) {
+//         return 'I was clicked.'
+//     })
+// }
 
 
 
@@ -16,6 +36,8 @@ function addingEventListener() {
 // //     alert('I was clicked!');
 // //     input.addEventListener('click', addingEventListener)
 // // }
+
+
 
 
 
@@ -33,4 +55,3 @@ function addingEventListener() {
 //     alert('I was clicked.');
 //     input.addEventListener('click', addingEventListener);
 // }
-
